3b006ef5367fbfca8116f0a025b8380f07d3781a
julong <345340585@qq.com>
2018-02-06 00:22:13 +0800

    Update pay.php

--- config/pay.php
+++ config/pay.php
@@ -12,7 +12,8 @@ return [
       'app_id' => "2018011301825418",
 
       //商户私钥，您的原始格式RSA私钥
-      'merchant_private_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/app_private_key.pem"),
+      'merchant_private_key' => '',  
+      //'merchant_private_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/app_private_key.pem"),  
 
       //异步通知地址
       'notify_url' => "http://工程公网访问地址/alipay.trade.wap.pay-PHP-UTF-8/notify_url.php",
@@ -30,7 +31,8 @@ return [
       'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
 
       //支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-      'alipay_public_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/zfb_public_key.pem"),
+      'alipay_public_key' => '',  
+      //'alipay_public_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/zfb_public_key.pem"),
 
   ],
     'wechat'=>[
