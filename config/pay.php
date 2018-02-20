126d2bb5a384c05974f6f48234e341fa89d91335
Lu qiang <345340585@qq.com>
2018-02-20 12:19:03 +0800

    加入微信支付

--- config/pay.php
+++ config/pay.php
@@ -11,9 +11,8 @@ return [
       //应用ID,您的APPID。
       'app_id' => "2018011301825418",
 
-      //商户私钥，您的原始格式RSA私钥
-      'merchant_private_key' => '',  
-      //'merchant_private_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/app_private_key.pem"),  
+      //商户私钥，您的原始格式RSA私钥   请替换成自己的，本处key内容为空
+      'merchant_private_key' => file_get_contents("./key/app_private_key.pem"),
 
       //异步通知地址
       'notify_url' => "http://工程公网访问地址/alipay.trade.wap.pay-PHP-UTF-8/notify_url.php",
@@ -31,11 +30,14 @@ return [
       'gatewayUrl' => "https://openapi.alipay.com/gateway.do",
 
       //支付宝公钥,查看地址：https://openhome.alipay.com/platform/keyManage.htm 对应APPID下的支付宝公钥。
-      'alipay_public_key' => '',  
-      //'alipay_public_key' => file_get_contents("/data/JULONG/JLSERVER/config/key/zfb_public_key.pem"),
+      //请替换成自己的，本处key内容为空
+      'alipay_public_key' => file_get_contents("./key/zfb_public_key.pem"),
 
   ],
     'wechat'=>[
-
+        'appid'=>'wxd678efh567hg6787',
+        'mch_id'=>'1230000109',
+        'key'=>'',
+        'notify_url'=>''
     ]
 ];
